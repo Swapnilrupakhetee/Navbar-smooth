30b3d2c6620819bf738f560e99612d1af9145dd7
Swapnilrupakhetee <rupakheteeswapnil@gmail.com>
2024-02-28 10:14:01 +0545

    file-creation

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+      div.inner-container
+
+    
+    </div>
+  )
+}
+
+export default Navbar
